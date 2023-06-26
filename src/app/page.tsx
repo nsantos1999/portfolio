c04fab7e6d6275d669597874ead6322eafbfd15f
Natã Santos <natasouza.santos50@gmail.com>
2023-06-26 16:33:41 -0300

    ⚰️ removing dead code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import { HomeContent } from "@/modules/home/contents/HomeContent";
 
--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -3,15 +3,10 @@
 import { styled } from "styled-components";
 
 export const Container = styled.div`
-  /* border-radius: 3%; */
   border-radius: 10px;
-  /* box-shadow: ${({ theme }) => theme.shadow.medium({})}; */
-  /* border: 1px solid ${({ theme }) => theme.palette.secondary.dark}; */
   padding: 1em;
-  /* background-color: ${({ theme }) => theme.palette.primary.light}; */
   background-image: linear-gradient(
     ${({ theme }) => theme.palette.primary.light},
     ${({ theme }) => theme.palette.primary.main}
   );
-  /* border: 1px solid transparent; */
 `;
